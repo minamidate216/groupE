b233bd72dcf6d8ec79ceb5fa2f28cac7b903037e
aso-arita <2201417@s.asojuku.ac.jp>
2023-11-17 11:25:08 +0900

    Create favorite-insert.php

---
+++ favorite-insert.php
@@ -0,0 +1,20 @@
+<?php require 'db-connect.php'; ?>
+<?php require 'header.php'; ?>
+<?php
+if(!isset($_SESSION['Users'])){
+    echo 'お気に入り機能の使用にはログインが必要です。<br>';
+    echo '<a href="login.php">ログインへ</a>';
+}else{
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->prepare('select * from Favorites where user_id=? and product_id = ?');
+$sql->execute([$_SESSION['Users']['user_id'],$_GET['id']]);
+if($sql->rowCount()==0){
+    $sql = $pdo->prepare('insert into Favorites values (?,?)');
+    $sql->execute([$_SESSION['Users']['user_id'],$_GET['id']]);
+    echo 'お気に入りに追加しました。';
+}else{
+    echo '既にお気に入りに追加済みです。';
+}
+require 'favorite.php';
+}
+?>
