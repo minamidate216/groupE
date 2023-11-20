b25b760551c86da0accbde218e1480f087b0c008
aso-arita <2201417@s.asojuku.ac.jp>
2023-11-20 14:10:38 +0900

    Create G1-2-8.php

---
+++ G1-2-8.php
@@ -0,0 +1,14 @@
+<?php require 'header.php'; ?>
+<?php
+if(isset($_SESSION['Users'])){
+    unset($_SESSION['Users']);
+    echo 'ログアウトしました。';
+    echo '<form action="G1-1-1.php">';
+    echo '<button type="submit">トップへ</button>';
+    echo '</form>';
+} else {
+    echo 'すでにログアウトしています。';
+}
+//そもそもif文いるか検証
+?>
+<?php require 'footer.php'; ?>
