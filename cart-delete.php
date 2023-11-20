083f9015cb6cf7573c8c892ec75fe617125b4e47
aso-arita <2201417@s.asojuku.ac.jp>
2023-11-20 14:11:26 +0900

    Delete cart-delete.php

--- cart-delete.php
+++
@@ -1,9 +0,0 @@
-<?php require 'db-connect.php'; ?>
-<?php require 'header.php'; ?>
-<?php
-unset($_SESSION['product'][$_GET['id']]);
-echo 'カートから削除しました。';
-echo '<hr>';
-?>
-<?php require 'cart.php'; ?>
-<?php require 'footer.php'; ?>
