a2d2c242a3c7c314b954b15341ccdebf578efeab
Iwamasai <148730551+Iwamasai@users.noreply.github.com>
2023-11-20 15:02:37 +0900

    Update G1-2-5.php

--- G1-2-5.php
+++ G1-2-5.php
@@ -3,20 +3,20 @@
 
 // フォームデータをセッションに保存
 if (isset($_POST['submit'])) {
-    $_SESSION['hozon']['user_name'] = $_POST['name'];
+    $_SESSION['hozon']['user_name'] = $_POST['user_name'];
     $_SESSION['hozon']['password'] = $_POST['password'];
     $_SESSION['hozon']['email'] = $_POST['email'];
     $_SESSION['hozon']['address'] = $_POST['address'];
 }
 
 // セッションからデータを取得
-$name = isset($_SESSION['hozon']['user_name']) ? $_SESSION['hozon']['user_name'] : '';
+$user_name = isset($_SESSION['hozon']['user_name']) ? $_SESSION['hozon']['user_name'] : '';
 $password = isset($_SESSION['hozon']['password']) ? $_SESSION['hozon']['password'] : '';
 $email = isset($_SESSION['hozon']['email']) ? $_SESSION['hozon']['email'] : '';
 $address = isset($_SESSION['hozon']['address']) ? $_SESSION['hozon']['address'] : '';
 
 // セッションから取得したデータを表示
-echo '<p>名前: ', $name, '</p>';
+echo '<p>名前: ', $user_name, '</p>';
 echo '<p>パスワード: ', $password, '</p>';
 echo '<p>メールアドレス: ', $email, '</p>';
 echo '<p>住所: ', $address, '</p>';
