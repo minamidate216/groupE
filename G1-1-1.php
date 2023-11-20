77abb89da9cd29fafb52b00738c5d5e315e397bc
minamidate216 <2201412@s.asojuku.ac.jp>
2023-11-20 16:23:11 +0900

    Update G1-1-1.php

--- G1-1-1.php
+++ G1-1-1.php
@@ -2,26 +2,45 @@
 <?php require 'db-connect.php'; ?>
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
-    <title>サイトトップ画面</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+<link rel="stylesheet" href="css/top.css">
+<title>Document</title>
 </head>
-
 <body>
+<div class="">
+<a href="G1-1-1.php"><img src="image/top.png" height="50px" width="50px">miyosi farm</a>
+<form action="G1-5-1.php" method="post">
+<input type="text" name="keyword">
+<button type="submit">検索</button>
+</form>
+<a href="G1-6-1.php">お気に入り</a>
+<a href="G1-7-1.php">注文履歴</a>
+<a href="G1-9-1.php">カート</a>
+<a href="G1-4-2.php">コラム</a>
+<?php
+if(!isset($_SESSION['Users'])){
+    echo '<a href="G1-2-1.php">ログイン</a>';
+}else{
+    echo '<a href="G1-2-7.php">ログアウト</a>';
+}
+?>
+</div>
+<hr>
 
-    <?php require 'header.php'; ?>
-
-    <img class="top_img" src="image/0.png" alt="">
+    <div class="image-container">
+        <img src="image/top.png" alt="Description">
+    </div>
     <div class="product">
         <h4 class="title">PRODUCTS</h4>
     </div>
     <div class="column">
         <ul>
             <div class="product_card">
-                <li><a href="G1-5-1.php?"  >
+                <li><a href="G1-5-1.php?">
                         <img class="img_link" src="image/0.png" alt="">
                         <p>商品一覧</p>
                     </a></li>
@@ -48,7 +67,7 @@
             <?php
             $sql = $pdo->query('select * from Columns');
             foreach ($sql as $row) {
-                echo '<li><a href="column.php?column=', $row['column_id'], '">';
+                echo '<li><a href="G1-4-1.php?column=', $row['column_id'], '">';
                 echo '<img class="img_link" src="image/', $row['post_img'], '" style="height:100px;"><br>';
                 echo '<p>', $row['column_title'], '</p>';
                 echo '</a></li>';
