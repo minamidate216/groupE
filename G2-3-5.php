d126f9f5741a80fe2cd6d05ade96ee7d3600ddb2
popopopo0721 <2201389@s.asojuku.ac.jp>
2023-11-27 15:49:35 +0900

    Update G2-3-5.php

--- G2-3-5.php
+++ G2-3-5.php
@@ -11,7 +11,7 @@ try {
 }
 
 try {
-    $query = "SELECT * FROM Products"; 
+    $query = "SELECT * FROM Columns"; 
     $stmt = $connect->query($query);
     $product = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
