0acbb111b24f6ee10f18054b9943da484e02dd5a
popopopo0721 <2201389@s.asojuku.ac.jp>
2023-11-27 16:49:59 +0900

    Update G2-3-1.php

--- G2-3-1.php
+++ G2-3-1.php
@@ -1,26 +1,22 @@
+<?php require 'db-connect.php'; ?>
 <?php require 'midasi.php'; ?>
+
 <?php
-// データベース接続情報
-$servername = "mysql215.phy.lolipop.lan";
-$username = "LAA1517365";
-$password = "Pass0916";
-$dbname = "LAA1517365-shop";
- 
 // データベース接続
-$conn = new mysqli($servername, $username, $password, $dbname);
- 
+$conn = new PDO($connect,USER, PASS);
+
 // 接続確認
-if ($conn->connect_error) {
+if ($conn) {
     die("データベース接続エラー: " . $conn->connect_error);
 }
- 
+
 // Columnsテーブルからデータ取得
-$sql = "SELECT c.column_title, a.admin_name, c.post_data, c.admin_id
+$sql = "SELECT c.column_id, c.column_title, a.admin_name, c.post_data, c.admin_id
         FROM Columns c
         INNER JOIN Admins a ON c.admin_id = a.admin_id";
 $result = $conn->query($sql);
- 
-if ($result->num_rows > 0) {
+
+if ($result->rowCount() > 0) {
     // テーブルヘッダー
     echo "<table border='1'>
             <tr>
@@ -30,22 +26,23 @@ if ($result->num_rows > 0) {
                 <th>更新</th>
                 <th>削除</th>
             </tr>";
- 
+
     // データ表示
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetchAll(PDO::FETCH_ASSOC)) {
         echo "<tr>
                 <td>" . $row["column_title"] . "</td>
                 <td>" . $row["admin_name"] . "</td>
                 <td>" . $row["post_data"] . "</td>
-                <td><a href='update.php?column_id=" . $row["admin_id"] . "'>更新</a></td>
-                <td><a href='delete.php?column_id=" . $row["admin_id"] . "'>削除</a></td>
+                <td><a href='G2-3-5kousin.php?column_id=" . $row["column_id"] . "'>更新</a></td>
+                <td><a href='delete.php?column_id=" . $row["column_id"] . "'>削除</a></td>
               </tr>";
     }
     echo "</table>";
 } else {
     echo "データがありません";
 }
- 
+
 $conn->close();
 ?>
+
 <button onclick="location.href='G2-3-2.php'">登録</button>
