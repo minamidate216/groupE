1eaa194ac2822b5b246451c641e4237b1f70ea83
popopopo0721 <2201389@s.asojuku.ac.jp>
2023-11-27 16:49:12 +0900

    Update G2-3-5kousin.php

--- G2-3-5kousin.php
+++ G2-3-5kousin.php
@@ -1,11 +1,24 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
-<?php $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8'; ?>
+<?php 
+if(!isset($_GET['column_id'])){
+    echo "不正なアクセスです";
+    exit();
+}
+// データベース接続
+$conn = new PDO($connect,USER, PASS);
+// Columnsテーブルからデータ取得
+$sql = "SELECT * FROM Columns WHERE column_id = ?";
+$result = $conn->prepare($sql);
+$result->excecute([$_GET['column_id']])
+$row = $result->fetch();
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=de
+    vice-width, initial-scale=1.0">
     <title>コラム更新画面</title>
     <style>
         body {
@@ -21,9 +34,9 @@
 <body>
     <form action="G2-3-5kakunin.php" method="post">
         <h1>コラム更新</h1>
-        <p>コラムタイトル <input type="text" name="column_title" maxlength="40"></p>
-        <p>画像 <input type="file" name="post_img"></p>
-        <p>本文 <input type="text" name="content"  maxlength="255"></textarea></p>
+        <p>コラムタイトル <input type="text" name="column_title" maxlength="40" value="<?= $row['column_title'] ?>"></p>
+        <p>画像 <input type="file" name="post_img" value="<?= $row['post_img'] ?>"></p>
+        <p>本文 <input type="text" name="content"  maxlength="255"> value="<?= $row['content'] ?>"</textarea></p>
         <a href="G2-3-1.php">保存せず戻る</a>
         <button type="submit">更新</button>
         
