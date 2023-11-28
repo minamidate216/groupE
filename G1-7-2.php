dd484feae3e830ba3244484005a50a97f4bf2cad
aso-arita <2201417@s.asojuku.ac.jp>
2023-11-28 11:03:04 +0900

    Update G1-7-2.php

--- G1-7-2.php
+++ G1-7-2.php
@@ -10,8 +10,10 @@ $sql = $pdo->prepare('SELECT p.description, p.product_img, p.product_id, p.produ
 $sql->execute([$_SESSION['Users']['user_id'],$_POST['orderId']]);
 if($sql->rowCount() > 0){
     $total=0;
-    echo '<table class="table is-striped">';
-    echo '<tr><th>注文番号：',$_POST['orderId'],'</th><th></th><th>購入日：',$_POST['Date'],'</th><th></th><th></th><th></th></tr>';
+    echo '<div class="columns ">';
+    echo '<div class="column is-two-third is-offset-2">';
+    echo '<table class="table is-striped column">';
+    echo '<tr class="has-background-success"><th>注文番号：',$_POST['orderId'],'</th><th></th><th>購入日：',$_POST['Date'],'</th><th></th><th></th><th></th></tr>';
 foreach($sql as $row){
     $date=date('Y-m-d', strtotime($row['date']));
     $price = $row['price']*$row['quantity'];
@@ -19,11 +21,11 @@ foreach($sql as $row){
     echo '<tr>';
     echo '<td><a href="G1-8-1.php?id=',$row['product_id'],'">';
     echo '<img alt="image" width="100" height="100" src="../image/', $row['product_img'], '"></a></td>';
-    echo '<td><a href="G1-8-1.php?id=',$row['product_id'],'">',$row['product_name'],'</a></td>';
-    echo '<td>',$row['price'],'円</td>';
-    echo '<td>',$row['quantity'],'点</td>';
-    echo '<td>',$price,'円</td>';
-    echo '<td>';
+    echo '<td class="is-vcentered"><a href="G1-8-1.php?id=',$row['product_id'],'">',$row['product_name'],'</a></td>';
+    echo '<td class="is-vcentered">',$row['price'],'円</td>';
+    echo '<td class="is-vcentered">',$row['quantity'],'点</td>';
+    echo '<td class="is-vcentered">',$price,'円</td>';
+    echo '<td class="is-vcentered">';
     echo '<form action="G1-9-1-insert.php" method="post">';
     echo '<input type="hidden" name=id value="',$row['product_id'], '">';
     echo '<input type="hidden" name=name value="',$row['product_name'], '">';
@@ -36,7 +38,7 @@ foreach($sql as $row){
     echo '</td>';
     echo '</tr>';
 }
-echo '<tr><th></th><th></th><th></th><th></th><th>お買い上げ金額：',$total,'円</th></tr>';
+echo '<tr><th></th><th></th><th></th><th></th><th></th><th>お買い上げ金額：',$total,'円</th></tr>';
 echo '</table>';
 }
 else{
