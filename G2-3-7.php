59a258c2d43d47e7849fe40834dd76270f35a573
shibayuu <2201364@s.asojuku.ac.jp>
2023-11-30 10:26:47 +0900

    Update G2-3-7.php

--- G2-3-7.php
+++ G2-3-7.php
@@ -1,28 +1,29 @@
-<?php session_start(); ?>
-<?php require 'db-connect.php'; ?>
-<?php 
-$connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8'; ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>コラム更新完了画面</title>
-    <style>
-        body {
-            text-align: center;
-        }
- 
-        form {
-            display: inline-block;
-            text-align: left;
-        }
-    </style>
-</head>
-<body>
-    <form action="G2-3-1.php" method="post">
-        <h3>コラムの更新を完了しました</h3>
-        <button type="submit">コラム管理画面へ</button>
-    </form>
-</body>
-</html>
+<?php
+session_start();
+require 'db-connect.php';
+
+// 商品IDが渡されていない場合はエラー表示
+if (!isset($_GET['product_id'])) {
+    die('商品IDが指定されていません。');
+}
+
+$productId = $_GET['product_id'];
+
+try {
+    $connect = new PDO($connect, USER, PASS);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // データベースから商品を削除
+    $query = "DELETE FROM Products WHERE product_id = :product_id";
+    $stmt = $connect->prepare($query);
+    $stmt->bindParam(':product_id', $productId);
+    $stmt->execute();
+
+    // 削除完了後、リダイレクト
+    header('Location: G2-3-6.php?delete_success=1&product_id=' . $productId);
+    exit();
+} catch (PDOException $e) {
+    die('データベースエラー: ' . $e->getMessage());
+}
+?>
+<a href="G2-2-1.php">戻る</a>
