459962d5ce01fb66f7e8074657dcf23e0d4023af
Iwamasai <148730551+Iwamasai@users.noreply.github.com>
2023-11-30 11:45:01 +0900

    Update G1-2-6.php

--- G1-2-6.php
+++ G1-2-6.php
@@ -8,22 +8,22 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // セッションからデータを取得
-    $user_id = isset($_SESSION['User']['user_id']) ? $_SESSION['User']['user_id'] : '';
     $user_name = isset($_SESSION['User']['user_name']) ? $_SESSION['User']['user_name'] : '';
-    $email = isset($_SESSION['User']['password']) ? $_SESSION['User']['password'] : '';
-    $password = isset($_SESSION['User']['email']) ? $_SESSION['User']['email'] : '';
+    $email = isset($_SESSION['User']['email']) ? $_SESSION['User']['email'] : '';
+    $password = isset($_SESSION['User']['password']) ? $_SESSION['User']['password'] : '';
     $address = isset($_SESSION['User']['address']) ? $_SESSION['User']['address'] : '';
 
-    // プリペアドステートメントを使用して安全にデータベースに挿入
-    $stmt = $pdo->prepare("INSERT INTO Users (user_id,user_name, password, email, address) VALUES (?,?, ?, ?, ?)");
-    $stmt->execute([$user_id,$user_name, $email, $password, $address]);
+    // プリペアドステートメントを使用してデータベースに挿入
+    $stmt = $pdo->prepare("INSERT INTO Users (user_name, email, password, address) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$user_name, $email, $password, $address]);
 
     // 登録完了メッセージを表示
     echo "登録が完了しました。";
-
+    echo '<form action="G1-2-1.php" method="post">';
+    echo '<input type="submit" value="トップへ">';
+    echo '</form>';
 } catch (PDOException $e) {
     echo "エラー: " . $e->getMessage();
-} finally {
     // データベース接続を閉じる
     $pdo = null;
 }
