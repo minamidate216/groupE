25972259f2ae827c248ebe82fde295daec65c15f
Iwamasai <148730551+Iwamasai@users.noreply.github.com>
2023-11-30 11:44:56 +0900

    Update G1-2-4.php

--- G1-2-4.php
+++ G1-2-4.php
@@ -1,78 +1,94 @@
-<?php require 'header.php'; ?>
+<?php session_start(); ?>
+<?php require 'db-connect.php'; ?>
 <?php
 $errors = [];
 
+$password = isset($_SESSION['User']['password']) ? $_SESSION['User']['password'] : '';
+$email = isset($_SESSION['User']['email']) ? $_SESSION['User']['email'] : '';
+$address = isset($_SESSION['User']['address']) ? $_SESSION['User']['address'] : '';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // 入力値を取得
-    $user_id = $_POST['user_id']; // フォームの中に 'user_id' がないので 'id' を使用
-
     $user_name = $_POST['user_name'];
     $password = $_POST['password'];
     $email = $_POST['email'];
     $address = $_POST['address'];
 
-    // エラーメッセージ
-    if (empty($user_name)) {
-        $errors[] = "氏名を入力してください。";
+    // 入力項目のチェック
+    $pdo = new PDO($connect, USER, PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //名前の重複チェック
+    $check_name = $pdo->prepare('SELECT COUNT(*) FROM Users WHERE user_name = ?');
+    $check_name->execute([$user_name]);
+    $check_name_count = $check_name->fetchColumn();
+    if ($check_name_count > 0) {
+        $errors[] = '入力された名前は既に使用されています。';
     }
-    if (empty($password)) {
-        $errors[] = "パスワードを入力してください。";
+    //emilの重複チェック
+    $check_email = $pdo->prepare('SELECT COUNT(*) FROM Users WHERE email = ?');
+    $check_email->execute([$email]);
+    $check_email_count = $check_email->fetchColumn();
+    if ($check_email_count > 0) {
+        $errors[] = '入力されたメールアドレスは既に使用されています。';
+    }   
+    if (empty($user_name)) {
+        $errors[] = '氏名を入力してください。';
     }
     if (empty($email)) {
-        $errors[] = "メールアドレスを入力してください。";
+        $errors[] = 'メールアドレスを入力してください。';
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = '正しい形式のメールアドレスを入力してください。';
+    }
+    if (empty($password)) {
+        $errors[] = 'パスワードを入力してください。';
     }
     if (empty($address)) {
-        $errors[] = "住所を入力してください。";
+        $errors[] = '住所を入力してください。';
     }
 
-    // エラーがなければ次の画面に遷移
+    // エラーがなければ次の処理へ
     if (empty($errors)) {
-        $_SESSION['User']['user_id'] = $user_id;
         $_SESSION['User']['user_name'] = $user_name;
         $_SESSION['User']['password'] = $password;
         $_SESSION['User']['email'] = $email;
         $_SESSION['User']['address'] = $address;
-
-        header("Location: G1-2-5.php");
-        exit();
+        header('Location: G1-2-5.php');
+        exit;
     }
 }
-
 ?>
+<?php require 'header.php'; ?>
+<?php
+echo '<form action="" method="post">';
+echo '<h3>会員登録</h3>';
+echo '<table>';
+echo '<tr>';
+echo '<td>氏名</td>';
+echo '<td><input type="text" name="user_name" value="' . $user_name . '"></td>';
+echo '</tr>';
+echo '<tr>';
+echo '<td>メールアドレス</td>';
+echo '<td><input type="text" name="email" value="' . $email . '"></td>';
+echo '</tr>';
+echo '<tr>';
+echo '<td>パスワード</td>';
+echo '<td><input type="password" name="password" value="' . $password . '"></td>';
+echo '</tr>';
+echo '<tr>';
+echo '<td>住所</td>';
+echo '<td><input type="text" name="address" value="' . $address . '"></td>';
+echo '</tr>';
+echo '</table>';
 
-<!-- エラーメッセージの表示 -->
-<?php if (!empty($errors)) : ?>
-    <ul style="color: red;">
-        <?php foreach ($errors as $error) : ?>
-            <li><?php echo $error; ?></li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
+if (!empty($errors)) {
+    echo '<ul style="color: red;">';
+    foreach ($errors as $error) {
+        echo '<li>', $error, '</li>';
+    }
+    echo '</ul>';
+}
 
-<!-- フォーム -->
-<form action="" method="post">
-    <table>
-        <tr>
-            <td>ID</td>
-            <td><input type="text" name="user_id"></td>
-        </tr>
-        <tr>
-            <td>氏名</td>
-            <td><input type="text" name="user_name"></td>
-        </tr>
-        <tr>
-            <td>メールアドレス</td>
-            <td><input type="text" name="email"></td>
-        </tr>
-        <tr>
-            <td>パスワード</td>
-            <td><input type="password" name="password"></td>
-        </tr>
-        <tr>
-            <td>住所</td>
-            <td><input type="text" name="address"></td>
-        </tr>
-    </table>
-    <!-- 送信ボタン -->
-    <input type="submit" name="submit" value="次へ">
-</form>
+echo '<input type="submit" name="submit" value="次へ">';
+echo '</form>';
+?>
