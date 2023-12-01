bc0d7b18dbc515451aae9634d88c5112da16ea1e
kawanoichiro <2201358@s.asojuku.ac.jp>
2023-12-01 11:53:13 +0900

    Update G2-3-2.php

--- G2-3-2.php
+++ G2-3-2.php
@@ -1,11 +1,9 @@
-<?php require 'midasi.php'; ?>
+<?php require 'header.php'; ?>
 <h2>新しいコラムを登録する</h2>
 <form action="G2-3-3.php" method="post" enctype="multipart/form-data">
-    コラムタイトル: <input type="text" name="column_title"><br>
-    画像ファイル: <input type="file" name="post_img"><br>
-    本文: <textarea name="content" rows="4" cols="50"></textarea><br>
-    更新日時: <input type="date" name="post_data"><br>
-    追加者ID: <input type="text" name="admin_id"><br>
+    コラムタイトル: <input type="text" name="column_title" required><br>
+    画像ファイル: <input type="file" name="post_img" required><br>
+    本文: <textarea name="content" rows="4" cols="50" maxlength="255" required></textarea><br>
     <input type="submit" value="登録する">
 </form>
 <?php require 'footer.php'; ?>
