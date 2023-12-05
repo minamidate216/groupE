b2abb5f3b2c20f299a8d8ac728dd5ea44f486a76
shibayuu <2201364@s.asojuku.ac.jp>
2023-12-05 10:48:21 +0900

    Update G2-2-2.php

--- G2-2-2.php
+++ G2-2-2.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 require 'db-connect.php';
 require 'header.php'; ?>
 
@@ -25,24 +24,46 @@ try {
 // データベースからカテゴリを取得
 $result = $connect->query("SELECT category FROM Category");
 $categories = $result->fetchAll(PDO::FETCH_ASSOC);
-$row = $result->fetch();
 ?>
-<form action="G2-2-3.php" method="post" enctype="mulitipart/form-data">
-        <h1>商品登録</h1>
-        <p>商品名 <input type="text" name="product_name" maxlength="30" value="<?= $row['product_name'] ?>"></p>
-        <p>商品価格 <input type="text" name="price" maxlength="10" value="<?= $row['price'] ?>"></p>
-        <p>説明 <input type="text" name="description" maxlength="500" value="<?= $row['description'] ?>"></p>
-        <p><img src="<?= $row['product_img'] ?>" alt="" width="200px">
-        <input type="hidden" name="product_img" value="<?= $row['product_img'] ?>"></p>
-        <p>画像 <input type="file" name="product_img" ></p>
-        <p>内容量 <input type="text" name="capacity" maxlength="5" value="<?= $row['capacity'] ?>"></p>
-    <label for="category">カテゴリー</label>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        /* Add custom styles here if needed */
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 50vh;
+            margin: 0;
+        }
+
+        form {
+            width: 50%; /* Adjust the width as needed */
+            margin-top: 50px; 
+        }
+    </style>
+</head>
+<body>
+<form action="G2-2-3.php" method="post" enctype="multipart/form-data">
+        <h1 class=has-text-primary-dark>商品登録</h1>
+        <p class=has-text-primary-dark>商品名 <input type="text" name="product_name" maxlength="30"kk></p>
+        <p class=has-text-primary-dark>商品価格 <input type="text" name="price" maxlength="10" ></p>
+        <p class=has-text-primary-dark>説明 <input type="text" name="description" maxlength="500"></p>
+        <p class=has-text-primary-dark>画像 <input type="file" name="product_img" ></p>
+        <p class=has-text-primary-dark>内容量 <input type="text" name="capacity" maxlength="5"></p>
+    <label for="category" class=has-text-primary-dark>カテゴリー</label>
     <select name="category" required>
         <?php foreach ($categories as $category) : ?>
             <option value="<?= htmlspecialchars($category['category']) ?>"><?= htmlspecialchars($category['category']) ?></option>
         <?php endforeach; ?>
     </select><br>
-    <p>在庫数 <input type="text" name="quantity" maxlength="8" value="<?= $row['quantity'] ?>"></p>
-    <input type="submit" value="新規登録">
+    <p class=has-text-primary-dark>在庫数 <input type="text" name="quantity" maxlength="8"></p>
+    <button class="button is-primary">新規登録</button>
 </form>
+</body>
+</html>
 <?php require 'footer.php'; ?>
