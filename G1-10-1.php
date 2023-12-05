d45cad3fe7e232311beee723f8bc6d07dba06ed7
minamidate216 <2201412@s.asojuku.ac.jp>
2023-12-05 11:40:43 +0900

    Update G1-10-1.php

--- G1-10-1.php
+++ G1-10-1.php
@@ -8,38 +8,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
     <title>注文確認画面</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
-    <style>
-        .product-card {
-            display: flex;
-            align-items: center;
-        }
 
-        .product-image {
-            width: 100px;
-            /* 画像のサイズ調整 */
-            margin-right: 15px;
-        }
-
-        .product-details {
-            flex-grow: 1;
-        }
-
-        .product-price {
-            font-size: 1.5em;
-            color: #3273dc;
-            /* Bulmaのリンク色 */
-        }
-
-        .product-quantity {
-            width: 3em;
-            text-align: center;
-        }
-
-        .summary-box {
-            text-align: right;
-            margin-top: 20px;
-        }
-    </style>
 </head>
 
 <body>
@@ -62,69 +31,120 @@
     }
     ?>
     </div>
+    <br>
+    <br>
     <hr>
-    <hr>
-    <?php
-    // 在庫の確認をします
-    // 配送日の取得
-    $deliveryDate = date('Y-m-d', strtotime('+2 days'));
-    ?>
 
+    <div class="content container">
 
-    <div id="vueApp">
-        <section>
-            <div class="container">
-                商品画像：商品名：価格：小計
-                <!-- 商品情報 -->
-                <div class="product-card" v-for="product in allData" :key="product.id">
-                    <img class="product-image" :src="'image/' + product.image" :alt="product.name"
-                        style="width: 100px;">
-                    <div class="productdetails">
-                        <p class="title is-4">{{ product.name }}</p>
-                        <p class="product-price">{{ product.price }}円</p>
+        <h1>購入確認</h1>
+        <div id="vueApp">
+            <section>
+                <div class="card">
+                    <table class="table is-fullwidth is-striped">
+                        <div class="card-header">
+                            <thead>
+                                <tr>
+                                    <th class="m*-3">商品画像</th>
+                                    <th>商品名</th>
+                                    <th>価格</th>
+                                    <th>個数</th>
+                                    <th>小計</th>
+                                </tr>
+                            </thead>
+                        </div>
+                        <tbody>
+                            <tr v-for="product in allData" :key="product.id">
+                                <th><img class="product-image" :src="'image/' + product.image" :alt="product.name"
+                                        style="width: 200px;"></th>
+                                <th class="subtitle">{{ product.name }}</th>
+                                <th class="product-price">{{ product.price }}円</th>
+                                <th>
+                                    <div class="field has-addons"><button class="button"
+                                            @click="increment(product.id)">+1</button>
+                                        <button class="button" style="pointer-events: none;">{{ product.count
+                                            }}個</button>
+                                        <button class="button" @click="decrement(product.id)">-1</button>
+                                    </div>
+                                </th>
+                                <th>{{ product.price * product.count }}円</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <br>
+                <br>
+                <hr>
+                <div class="card">
+                    <header class="card-header">
+                        <span class="icon">
+                            <i class="fas fa-shipping-fast" aria-hidden="true"></i>
+                        </span>
+                        <p class="card-header-title">
+                            配送日
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            <h3>
+                                <?php
+                                $deliveryDate = date('Y-m-d', strtotime('+2 days'));
+                                echo $deliveryDate ?>
+                            </h3>
+                            <br><br>
+                        </div>
                     </div>
-                    <button class="button" @click="increment(product.id)">+1</button>
-                    {{ product.count }}個
-                    <button class="button" @click="decrement(product.id)">-1</button>
-                    {{ product.price * product.count }}円
                 </div>
-            </div>
-
-            <div class="field">
-                <label class="label">配送日</label>
-                <div class="control">
-                    <h3>
-                        <? echo $deliveryDate ?>
-                    </h3>
+                <!-- 購入者情報 -->
+                <br>
+                <br>
+                <hr>
+                <div class="card">
+                    <header class="card-header">
+                        <span class="icon">
+                            <i class="far fa-id-card" aria-hidden="true"></i>
+                        </span>
+                        <p class="card-header-title">
+                            購入者情報
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            <strong>
+                                <?php if (isset($_SESSION['Users'])) {
+                                    echo '<div>';
+                                    echo '<h3>名前：', $_SESSION['Users']['user_name'], '</h3>';
+                                    echo '<h3>住所：', $_SESSION['Users']['address'], '</h3>';
+                                    echo '<h3>連絡先：', $_SESSION['Users']['email'], '</h3>';
+                                    echo '<div>';
+                                }
+                                ?>
+                            </strong>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="field">
 
-                <?php if (isset($_SESSION['Users'])) {
-                    echo '<div>';
-                    echo '<p>名前：', $_SESSION['Users']['user_name'], '</p>';
-                    echo '<p>住所：', $_SESSION['Users']['address'], '</p>';
-                    echo '<p>連絡先：', $_SESSION['Users']['email'], '</p>';
-                    echo '<div>';
-                }
-                ?>
+                <br><br>
+            </section>
+            <div class="has-text-right">
+                <br>
+                <br>
+                <h1>合計金額</h1>
+                <h3>{{ Total }}円</h3>
+                <td>{{ message }}</td>
+                <form action="G1-10-2.php" method="post">
+                    <div v-for="product in allData" :key="product.id">
+                        <input type="hidden" :name="'productCount[' + product.id + ']'" :value="product.count">
+                        <input type="hidden" :name="'productId[' + product.id + ']'" :value="product.id">
+                        <input type="hidden" :name="'productName[' + product.id + ']'" :value="product.name">
+                        <input type="hidden" :name="'productPrice[' + product.id + ']'" :value="product.price">
+                        <input type="hidden" :name="'productImage[' + product.id + ']'" :value="product.image">
+                    </div>
+                    <input class="button is-dark" type="submit" value="購入">
+                </form>
             </div>
-            <div class="total">
-                <p>合計金額</p>
-                <p>{{ Total }}</p>
-            </div>
-        </section>
-        <td>{{ message }}</td>
-        <form action="G1-10-2.php" method="post">
-            <div v-for="product in allData" :key="product.id">
-                <input type="hidden" :name="'productCount[' + product.id + ']'" :value="product.count">
-                <input type="hidden" :name="'productId[' + product.id + ']'" :value="product.id">
-                <input type="hidden" :name="'productName[' + product.id + ']'" :value="product.name">
-                <input type="hidden" :name="'productPrice[' + product.id + ']'" :value="product.price">
-                <input type="hidden" :name="'productImage[' + product.id + ']'" :value="product.image">
-            </div>
-            <input type="submit" value="購入">
-        </form>
+        </div>
     </div>
     <!-- Vue.jsを適用するセクション -->
     <script>var productFromPHP = <?php echo json_encode($_SESSION['product']); ?>;
