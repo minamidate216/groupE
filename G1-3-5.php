e5e4f56cf93eab127b11d3e34224c11c84c98b63
Iwamasai <148730551+Iwamasai@users.noreply.github.com>
2023-12-05 09:32:27 +0900

    Update G1-3-5.php

--- G1-3-5.php
+++ G1-3-5.php
@@ -2,15 +2,16 @@
 <?php require 'db-connect.php'; ?>
 <?php
 //フォームの情報を取得
-$user_id = isset($_POST['user_id']) ? $_POST['user_id'] : '';
-$user_name = isset($_POST['user_name']) ? $_POST['user_name'] : '';
-$password = isset($_POST['password']) ? $_POST['password'] : '';
-$email = isset($_POST['email']) ? $_POST['email'] : '';
-$address = isset($_POST['address']) ? $_POST['address'] : '';
+$user_id = isset($_SESSION['User']['user_id']) ? $_SESSION['User']['user_id'] : '';
+$user_name = isset($_SESSION['User']['user_name']) ? $_SESSION['User']['user_name'] : '';
+$password = isset($_SESSION['User']['password']) ? $_SESSION['User']['password'] : '';
+$email = isset($_SESSION['User']['email']) ? $_SESSION['User']['email'] : '';
+$address = isset($_SESSION['User']['address']) ? $_SESSION['User']['address'] : '';
 
 //セッションに保存された情報を表示
 echo 'マイページ情報更新確認';
 echo '<table>';
+echo '<tr><td>ID</td><td>', $user_id, '</td></tr>';
 echo '<tr><td>氏名</td><td>', $user_name, '</td></tr>';
 echo '<tr><td>メールアドレス</td><td>', $email, '</td></tr>';
 echo '<tr><td>パスワード</td><td>', str_repeat('*', strlen($password)), '</td></tr>';
@@ -19,11 +20,6 @@ echo '</table>';
 
 // 更新画面に情報を渡すフォーム
 echo '<form action="G1-3-6.php" method="post">';
-echo '<input type="hidden" name="user_id" value="', $user_id, '">';
-echo '<input type="hidden" name="user_name" value="', $user_name, '">';
-echo '<input type="hidden" name="email" value="', $email, '">';
-echo '<input type="hidden" name="password" value="', $password, '">';
-echo '<input type="hidden" name="address" value="', $address, '">';
 echo '<input type="submit" value="更新する">';
 echo '</form>';
 
