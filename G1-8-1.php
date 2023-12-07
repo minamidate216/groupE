16b562a3dd4611d75c96eb1ec4aa42c9b7dbc3d9
minamidate216 <2201412@s.asojuku.ac.jp>
2023-12-07 18:05:25 +0900

    Update G1-8-1.php
    サイトトップ

--- G1-8-1.php
+++ G1-8-1.php
@@ -4,70 +4,75 @@
 <br>
 <br>
 <?php
-$sql = new PDO($connect, USER, PASS);
-// お気に入りに登録されているかどうかの確認
-$favoriteSql = $sql->prepare('select count(*) from Favorites where user_id=? and product_id = ?');
-$favoriteSql->execute([$_SESSION['Users']['user_id'], $_GET['id']]);
-$count = $favoriteSql->fetchColumn();
-if ($count > 0) {
-    $isFavorite = true;
-
+if (empty($_SESSION['Users'])) {
+    echo 'ログインしてください。<br>';
+    echo '<a href="G1-2-1.php">ログインへ';
 } else {
-    $isFavorite = false;
-}
 
-
-
-
-
-
-// 商品情報の取得
-$productId = $_GET['id'];
-$productSql = $sql->prepare('select * from Products where product_id=?');
-$productSql->execute([$productId]);
-foreach ($productSql as $row) {
-    echo '<form action="G1-9-1-insert.php" method="post">';
-    echo '<div class="columns">';
-    echo '<div class="column is">';
-    echo '<div class="card ml-6 mb-6" style="width: 300px";><div class="card-image"><figure class="image"><img alt="image" src="image/', $row['product_img'], '" style="width:300px";></figure></div></div>';
-    echo '</div>';
-    echo '<div class="media-content">';
-    // 中央の写真
-    echo '<img alt="image" src="image/', $row['product_sub_img'], '" 
-    style="width:450px; border-radius: 8px";></div>';
-    echo '<div class="media-right" style="width:30%; margin-right:20px";>';
-    // 右側の商品情報とカートボタンとハートボタン
-    echo '<div class="box">';
-    echo '<h3 class="title has-text-primary-dark">', $row['product_name'], '</h3>';
-    echo '<p class="subtitle has-text-right has-text-primary-dark" >', $row['price'], '円</p><br>';
-    echo '<p class="subtitle has-text-right has-text-primary-dark" >', $row['capacity'], '</p>';
-    echo '<div class="select is-medium is-rounded"><select name="count">';
-
-    var_dump($row['quantity']);
-    var_dump($_SESSION['product']);
-    for ($i = 1; $i <= $row['quantity']; $i++) {
-        echo '<option value="', $i, '">', $i, '</option>';
+    $pdo = new PDO($connect, USER, PASS);
+    // お気に入りに登録されているかどうかの確認
+    $favoriteSql = $pdo->prepare('select count(*) from Favorites where user_id=? and product_id = ?');
+    $favoriteSql->execute([$_SESSION['Users']['user_id'], $_GET['id']]);
+    $count = $favoriteSql->fetchColumn();
+    
+    if ($count > 0) {
+        $isFavorite = true;
+        
+    } else {
+        $isFavorite = false;
     }
-    echo '</select>  個</p>';
-    echo '</div>';
-    echo '</div>';
-    echo '<input type="hidden" name="id" value="', $row['product_id'], '">';
-    echo '<input type="hidden" name="name" value="', $row['product_name'], '">';
-    echo '<input type="hidden" name="price" value="', $row['price'], '">';
-    echo '<input type="hidden" name="description" value="', $row['description'], '">';
-    echo '<input type="hidden" name="image" value="', $row['product_img'], '">';
-    echo '<input type="hidden" name="quantity" value="', $row['quantity'], '">';
-    echo '<h5 class="subtitle ml-6">', $row['description'], '</h5>';
-    echo '<p><input class="ml-6" type="submit" value="カートに追加"></p>';
-    echo '</form>';
-}
-
-
-?>
+    
+    
+    // 商品情報の取得
+    $productId = $_GET['id'];
+    $productSql = $pdo->prepare('select * from Products where product_id=?');
+    $productSql->execute([$productId]);
+    echo '<div class="content">';
+    echo '<div class="container is-fluid">';
+    foreach ($productSql as $row) {
+        echo '<form action="G1-9-1-insert.php" method="post">';
+        echo '<article class="media">';
+        echo '<div class="media-left">';
+        //  左側の写真と説明
+        echo '<img alt="image" src="image/', $row['product_img'], '" style="width:450px; padding-left: 20px;
+        padding-bottom: 20px; border-radius: 8px";>';
+        echo '<h3 class="title"  style="width:500px";>', $row['description'], '</h3>';
+        echo '</div>';
+        echo '<div class="media-content">';
+        // 中央の写真
+        echo '<img alt="image" src="image/', $row['product_sub_img'], '" 
+        style="width:450px; border-radius: 8px";></div>';
+        echo '<div class="media-right" style="width:30%; margin-right:20px";>';
+        // 右側の商品情報とカートボタンとハートボタン
+        echo '<div class="box">';
+        echo '<h3 class="title has-text-primary-dark">', $row['product_name'], '</h3>';
+        echo '<p class="subtitle has-text-right has-text-primary-dark" >', $row['price'], '円</p><br>';
+        echo '<p class="subtitle has-text-right has-text-primary-dark" >', $row['capacity'], '</p>';
+        echo '<div class="select is-medium is-rounded"><select name="count">';
+        for ($i = 1; $i <= $row['quantity']; $i++) {
+            echo '<option value="', $i, '" >', $i, '個</option>';
+        }
+        echo '</select></div></div><br>';
+        echo '<input class="button is-info is-light" type="submit" value="カートに追加" style="margin: 30px";>';
+        echo '<input type="hidden" name="id" value="', $row['product_id'], '">';
+        echo '<input type="hidden" name="name" value="', $row['product_name'], '">';
+        echo '<input type="hidden" name="price" value="', $row['price'], '">';
+        echo '<input type="hidden" name="description" value="', $row['description'], '">';
+        echo '<input type="hidden" name="image" value="', $row['product_img'], '">';
+        echo '<input type="hidden" name="quantity" value="', $row['quantity'], '">';
+        echo '</form>';
+    }
+    
+    ?>
 <div id="vueApp">
     <!-- お気に入りボタン -->
-    <i :class="{'fas fa-heart': isFavorite, 'far fa-heart': !isFavorite}" @click="toggleFavorite"></i>
+    <i :class="{'fas fa-heart is-right': isFavorite, 'far fa-heart is-right': !isFavorite}" @click="toggleFavorite"
+    style=font-size:150px;color:#a3ffa3;></i>
 </div>
+</article>
+</div>
+</div>
+
 
 
 
@@ -80,4 +85,4 @@ foreach ($productSql as $row) {
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="./script/script.js"></script>
 
-<?php require 'footer.php'; ?>
+<?php require 'footer.php'; }?>
