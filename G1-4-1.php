61e108aa0f086aed71848c31cc0ac017190ef27c
minamidate216 <2201412@s.asojuku.ac.jp>
2023-12-07 14:54:39 +0900

    Update G1-4-1.php

--- G1-4-1.php
+++ G1-4-1.php
@@ -1,5 +1,5 @@
-<?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
+
 <?php
 echo '<h1 class="has-text-centered is-size-2">Miyoshi Columns</h1><br><br>';
 $pdo = new PDO($connect, USER, PASS);
