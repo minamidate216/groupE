f56ffcc15efea7ea1ba9d4ad153a0f150ad55515
Iwamasai <148730551+Iwamasai@users.noreply.github.com>
2023-12-08 12:29:49 +0900

    Update G1-3-5.php

--- G1-3-5.php
+++ G1-3-5.php
@@ -1,5 +1,4 @@
 <?php require 'header.php'; ?>
-
 <?php
 //フォームの情報を取得
 $user_id = isset($_SESSION['User']['user_id']) ? $_SESSION['User']['user_id'] : '';
@@ -9,22 +8,43 @@ $email = isset($_SESSION['User']['email']) ? $_SESSION['User']['email'] : '';
 $address = isset($_SESSION['User']['address']) ? $_SESSION['User']['address'] : '';
 
 //セッションに保存された情報を表示
-echo 'マイページ情報更新確認';
-echo '<table>';
-echo '<tr><td>氏名</td><td>', $user_name, '</td></tr>';
-echo '<tr><td>メールアドレス</td><td>', $email, '</td></tr>';
-echo '<tr><td>パスワード</td><td>', str_repeat('*', strlen($password)), '</td></tr>';
-echo '<tr><td>住所</td><td>', $address, '</td></tr>';
-echo '</table>';
+echo '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">';
+echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">';
+echo '<div class="content">';
+    echo '<div class="container">';
+        echo '<div class="has-text-centered">';
+        echo '<h3>マイページ情報更新確認</h3>';
+        echo '</div>';
+        echo '<table>';
+        echo '<tr class="column is-half is-offset-4"><td class="has-text-primary">氏名　　　　　　</td><td>', $user_name, '</td></tr>';
+        echo '<tr class="column is-half is-offset-4"><td class="has-text-primary">メールアドレス　</td><td>', $email, '</td></tr>';
+        echo '<tr class="column is-half is-offset-4"><td class="has-text-primary">パスワード　　　</td><td>', str_repeat('*', strlen($password)), '</td></tr>';
+        echo '<tr class="column is-half is-offset-4"><td class="has-text-primary">住所　　　　　　</td><td>', $address, '</td></tr>';
+        echo '</table>';
+
+            echo '<div class="has-text-centered">';
+            echo '<h4>上記の内容に変更します。</h4>';
+            echo '</div>';
+        echo '</div>';    
+    echo '</div>';
+echo '</div>';
 
 // 更新画面に情報を渡すフォーム
-echo '<form action="G1-3-6.php" method="post">';
-echo '<input type="submit" value="更新する">';
-echo '</form>';
 
-echo '<form action="G1-3-4.php" method="post">';
-echo '<input type="submit" value="戻る">';
-echo '</form>';
+?>
+    <?php
+    $backURL = $_SERVER['HTTP_REFERER']; // 前のページのURLを取得
+    ?>
+    <form action="G1-3-6.php" method="post">
+        <div class="has-text-centered">
+        <a class="button is-primary" href="<?php echo $backURL; ?>">　戻る　</a>
+        　　　　　　
+        <input class="button is-primary" type="submit" value="変更する">
+        </form>
+        </div> 
+    </div> 
 
+
+<?php
 require 'footer.php';
 ?>
