a9ef01815548a50831d1e7bb149acd05b2bb867b
Iwamasai <148730551+Iwamasai@users.noreply.github.com>
2023-12-08 11:50:47 +0900

    Update G1-3-3.php

--- G1-3-3.php
+++ G1-3-3.php
@@ -1,6 +1,5 @@
+<?php require 'header.php'; ?>
 <?php
-require 'header.php';
-
 // セッションからユーザーIDを取得
 $user_id = isset($_SESSION['Users']['user_id']) ? $_SESSION['Users']['user_id'] : null;
 
@@ -17,24 +16,34 @@ if ($user_id) {
 
         if ($user) {
             // ユーザー情報を表示
-            echo 'マイページ';
+echo '<div class="content">';
+    echo '<div class="container">';
+        echo '<div class="has-text-centered">';
+            echo '<h3>マイページ</h3>';
+        echo '</div>';
             echo '<form action="G1-3-4.php" method="post">';
             echo '<table>';
-            echo '<tr><td>氏名</td><td>', $user['user_name'], '</td></tr>';
-            echo '<tr><td>メールアドレス</td><td>', $user['email'], '</td></tr>';
-            echo '<tr><td>パスワード</td><td>', str_repeat('*', strlen($user['password'])), '</td></tr>';
-            echo '<tr><td>住所</td><td>', $user['address'], '</td></tr>';
+            echo '<tr class="column is-half is-offset-4"><td class="has-text-primary">氏名　　　　　　</td><td>', $user['user_name'], '</td></tr>';
+            echo '<tr class="column is-half is-offset-4"><td class="has-text-primary">メールアドレス　</td><td>', $user['email'], '</td></tr>';
+            echo '<tr class="column is-half is-offset-4"><td class="has-text-primary">パスワード　　　</td><td>', str_repeat('*', strlen($user['password'])), '</td></tr>';
+            echo '<tr class="column is-half is-offset-4"><td class="has-text-primary">住所　　　　　　</td><td>', $user['address'], '</td></tr>';
+            
             echo '</table>';
-            echo '<input type="submit" value="登録内容をを変更する">';
+            echo '<div class="has-text-centered">';
+            echo '<input class="button is-primary" type="submit" value="登録内容をを変更する">';
+            echo '</div>';
             echo '</form>';
+        echo '</div>';
+    echo '</div>';
+echo '</div>';
         } else {
-            echo 'ユーザーが見つかりません。';
+            echo 'ログインしてください。';
         }
 
     } catch (PDOException $e) {
         echo "エラー: " . $e->getMessage();
     }
 } else {
-    echo 'ユーザーIDが保存されていません。';
+    echo 'ログインしてください。';
 }
 ?>
