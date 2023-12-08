0f5a210357a4e24bbc7b319f66558bec3bc9bc57
aso-arita <2201417@s.asojuku.ac.jp>
2023-12-08 12:13:38 +0900

    Update G1-2-7.php

--- G1-2-7.php
+++ G1-2-7.php
@@ -5,7 +5,7 @@ if(isset($_SESSION['Users'])){
     echo '<hr>';
     echo '<p class="is-size-4 has-text-centered" style="margin: 30px ">ログアウトしますか？</p>';
     echo '<form action="G1-2-8.php">';
-    echo '<button type="submit" style=" margin: 40px 500px 0px 535px">ログアウト</button>';
+    echo '<button type="submit" style=" margin: 40px 500px 0px 590px">ログアウト</button>';
     echo '</form>';
 }else{
     echo '<h1 class="is-size-2 has-text-centered">ログインしていません。';
