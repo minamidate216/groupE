744ad5c61deb39fc93acfb63be70c75e285134b2
minamidate216 <2201412@s.asojuku.ac.jp>
2023-12-11 15:56:13 +0900

    Update G1-8-3.php

--- G1-8-3.php
+++ G1-8-3.php
@@ -1,5 +1,8 @@
 <?php require 'header.php'; ?>
-
+<!-- 定期購入確認画面　-->
+<!-- 定期購入確認画面での入力を表示
+ユーザーが確認をして送信を押すとデータベースに挿入する
+遷移先はG1-8-3.php -->
 <br>
 <br>
 <br>
@@ -7,12 +10,6 @@
 
 <?php
 $pdo = new PDO($connect, USER, PASS);
-// $post_id = $_POST['id'];
-// $post_name = $_POST['name'];
-// $post_img = $_POST['img'];
-// $post_interval = $_POST['interval'];
-// $post_next_order_date = $_POST['next_order_date'];
-// $post_order_count = $_POST['order_count'];
 
 $user_id = $_SESSION['Users']['user_id'];
 $product_id = $_POST['id'];
@@ -34,5 +31,32 @@ echo '<p>ユーザーID: ' . $user_id . '</p>';
 echo '<p>間隔: ' . $post_interval . '</p>';
 echo '<p>次の注文日: ' . $post_next_order_date . '</p>';
 echo '<p>注文数: ' . $post_order_count . '</p>';
+
+?>
+
+
+<h1>定期購入確認画面</h1>
+<div class="box">
+  <div class="field">
+    <label class="label">商品名</label>
+    <div class="control">
+      
+    </div>
+  </div>
+ 
+  <div class="field">
+    <label class="label">商品画像</label>
+    <div class="control" image>
+      <img src="image/',$product_img,'" >
+    </div>
+  </div>
+ 
+  <button class="button is-primary">サインイン</button>
+</div>
+
+<form action="G1-8-4.php" method="post">
+    
+
+</form>
 
 
