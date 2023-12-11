7ed9c515d9885010f3ab92f7fbbfadb49e74eb3e
minamidate216 <2201412@s.asojuku.ac.jp>
2023-12-11 15:55:51 +0900

    Update G1-8-1.php

--- G1-8-1.php
+++ G1-8-1.php
@@ -8,32 +8,41 @@ if (empty($_SESSION['Users'])) {
     echo 'ログインしてください。<br>';
     echo '<a href="G1-2-1.php">ログインへ';
 } else {
-    $description="";
+    $description = "";
+    $productId = $_GET['id'];
     $isFavorite = false;
     $pdo = new PDO($connect, USER, PASS);
     // お気に入りに登録されているかどうかの確認
     $favoriteSql = $pdo->prepare('select count(*) from Favorites where user_id=? and product_id = ?');
     $favoriteSql->execute([$_SESSION['Users']['user_id'], $_GET['id']]);
     $count = $favoriteSql->fetchColumn();
-    $cartStock;
+    // if(empty($_SESSION['product'])){
+    //     $cartStock = 0;
+    // } elseif(!empty($_SESSION['product'])){
+    //     foreach($_SESSION['product'] as $id => $product){
+    //         if()
+    //     }
+    // }
     $purchaseStock = 0;
+    $cartStock = 0;
     $message = '<a href="G1-5-1.php" class="subtitle has-text-danger">売り切れ   商品一覧へ<i class="fas fa-reply"></i></a>';
-
     if ($count > 0) {
         $isFavorite = true;
     }
     // カートのセッション情報に表示されている商品と同じものがあれば在庫を取得する。
-    if (!empty($_SESSION['product'])) {
+    if (isset($_SESSION['product'][$productId])) {
+        /*
         foreach ($_SESSION['product'] as $id => $row) {
-            if ($id == $_GET['id']) {
+            if ($id == $productId) {
                 $cartStock = $row['count'];
             }
         }
+        */
+        $cartStock = $_SESSION['product'][$productId]['count'];
     }
 
 
     // 商品情報の取得
-    $productId = $_GET['id'];
     $productSql = $pdo->prepare('select * from Products where product_id=?');
     $productSql->execute([$productId]);
     echo '<div class="content">';
@@ -41,15 +50,17 @@ if (empty($_SESSION['Users'])) {
     foreach ($productSql as $row) {
         $purchaseStock = $row['quantity'];
         $description = $row['description'];
-        if (!empty($cartStock)) {
-            if (($purchaseStock - $cartStock) > 0 && ($purchaseStock - $cartStock) < 10) {
-                $purchaseStock -= $cartStock;
-                $message = '在庫残り' . $purchaseStock . '個となります。';
-            } elseif (($purchaseStock - $cartStock) >= 10) {
-                $purchaseStock = 10;
-            } elseif (($purchaseStock - $cartStock) <= 0) {
-                $purchaseStock = 0;
-                $message = '<a href="G1-9-1-show.php" class="subtitle has-text-danger">カート内の個数が最終在庫となります。    ＝＞<span class="icon is-size-4"><i
+        $maxPurchaseStock = $purchaseStock;
+        if ($cartStock > 0) {
+            $maxPurchaseStock = $purchaseStock - $cartStock;
+        if ($maxPurchaseStock > 0 && $maxPurchaseStock < 10) {
+            $message = '在庫残り' . $maxPurchaseStock . '個となります。';
+            echo $message;
+        } elseif ($maxPurchaseStock >= 10) {
+            $maxPurchaseStock = 10;
+        } elseif ($maxPurchaseStock <= 0) {
+            $maxPurchaseStock = 0;
+            $message = '<a href="G1-9-1-show.php" class="subtitle has-text-danger">カート内の個数が最終在庫となります。    <span class="icon is-size-4"><i
                 class="fas fa-shopping-cart"></i></span></a>';
             }
         }
@@ -71,17 +82,17 @@ if (empty($_SESSION['Users'])) {
         echo '<h3 class="title has-text-primary-dark">', $row['product_name'], '</h3>';
         echo '<p class="subtitle has-text-right has-text-primary-dark" >', $row['price'], '円</p><br>';
         echo '<p class="subtitle has-text-right has-text-primary-dark" >', $row['capacity'], '</p>';
-        if ($purchaseStock < 10 && $purchaseStock > 0) {
+        if ($maxPurchaseStock < 10 && $maxPurchaseStock > 0) {
             // 在庫が10より少ない時
-            echo '<p class="subtitle has-text-danger">', $message, '</p>';
+            echo '<p class="subtitle has-text-danger">商品の残り在庫は、', $maxPurchaseStock, '個です。</p>';
             echo '<div class="select is-medium is-rounded"><select name="count">';
-            for ($i = 1; $i <= $purchaseStock; $i++) {
+            for ($i = 1; $i <= $maxPurchaseStock; $i++) {
                 echo '<option value="', $i, '" >', $i, '個</option>';
             }
             echo '</select></div></div><br>';
             echo '<input class="button is-success is-light" type="submit" value="カートに追加" style="display: block; margin-left: auto";><br><br>';
-            echo '<a class="button is-primary is-dark" href="G1-8-2.php?id=',$productId,'" style="width:30%; display: block; margin-left: auto";>定期購入</a>';
-        } elseif ($purchaseStock >= 10) {
+            echo '<a class="button is-primary is-dark" href="G1-8-2.php?id=', $productId, '" style="width:30%; display: block; margin-left: auto";>定期購入</a>';
+        } elseif ($maxPurchaseStock >= 10) {
             // 在庫がカートの中の商品数を引いても10以上の時
             echo '<div class="select is-medium is-rounded"><select name="count">';
             for ($i = 1; $i <= 10; $i++) {
@@ -89,8 +100,8 @@ if (empty($_SESSION['Users'])) {
             }
             echo '</select></div></div><br>';
             echo '<input class="button is-success is-light" type="submit" value="カートに追加" style="display: block; margin-left: auto";><br><br>';
-            echo '<a class="button is-primary is-dark" href="G1-8-2.php?id=',$productId,'" style="width:30%; display: block; margin-left: auto";>定期購入</a>';
-        } elseif ($purchaseStock <= 0) {
+            echo '<a class="button is-primary is-dark" href="G1-8-2.php?id=', $productId, '" style="width:30%; display: block; margin-left: auto";>定期購入</a>';
+        } elseif ($maxPurchaseStock <= 0) {
             // 在庫がない,もしくはカートの中に入れている個数で在庫終了の時は、カートに追加ボタンは消しておいて商品一覧とカートへのリンクを表示
             echo '<div>', $message, '</div></div><br>';
         }
@@ -115,7 +126,7 @@ if (empty($_SESSION['Users'])) {
             style=font-size:80px;color:#00533f;text-align:right;></i>
     </div>
     </article>
-    <?php echo '<h3 class="sub-title" style="width:820px; margin-right:10px";>', $description, '</h3>';?>
+    <?php echo '<h3 class="sub-title" style="width:820px; margin-right:10px";>', $description, '</h3>'; ?>
     </div>
     </div>
 
@@ -124,8 +135,8 @@ if (empty($_SESSION['Users'])) {
 
     <!-- script.jsに商品とお気に入りの情報を渡す⇩ -->
     <script>
-        var productFromPHP = <?php echo json_encode($productId); ?>;
-        var favoriteFromPHP = <?php echo json_encode($isFavorite); ?>;
+            var productFromPHP = <?php echo json_encode($productId); ?>;
+            var favoriteFromPHP = <?php echo json_encode($isFavorite); ?>;
     </script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
