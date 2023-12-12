3e2b883e152a3474d4d3b6e5619eeacd03f909e0
minamidate216 <2201412@s.asojuku.ac.jp>
2023-12-13 02:39:39 +0900

    Update G1-8-4.php

--- G1-8-4.php
+++ G1-8-4.php
@@ -20,26 +20,37 @@
     $order_count = $_POST['order_count'];
     // echo '<p>画面表示</p>';
     // echo '<p>ID: ' . $product_id . '</p>';
-
+    
 
     // echo '<p>データベース用</p>';
     // echo '<p>ユーザーID: ' . $user_id . '</p>';
     // echo '<p>間隔: ' . $interval . '</p>';
     // echo '<p>次の注文日: ' . $next_order_date . '</p>';
     // echo '<p>注文数: ' . $order_count . '</p>';
-
-
-    try {
+    
+    // G1-8-6からの値によって実行するsqlを変更する
+    
+    if (isset($_POST['action']) && $_POST['action'] === 'delete') {
+        // 削除のsqlとメッセージを変数に格納
+    } elseif (isset($_POST['action']) && $_POST['action'] === 'update') {
+        // 更新sqlとメッセージを変数に格納
+    } else {
+        // 注文のinsert文とメッセージをsqlに格納
         // 定期購入情報　⇨　定期購入テーブル
         $sql = "INSERT INTO subscription_orders (subscription_id, user_id, product_id, interval_days, next_order_date, order_count) VALUES (NULL ,?, ?, ?, ?, ?)";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$user_id, $product_id, $interval, $next_order_date, $order_count]);
 
         // 挿入成功時のメッセージ
-        echo '<h1 class="title">ご注文が完了しました。<br>
-    ありがとうございます</h1>';
-        echo '<p class="subtitle">初回配送日以降に注文が確定し履歴に表示されます。</p>';
+        $title =  '<h1 class="title">ご注文が完了しました。<br>
+           ありがとうございます</h1>';
+        $subtitle =  '<p class="subtitle">初回配送日以降に注文が確定し履歴に表示されます。</p>';
+    }
 
+
+    try {
+
+        // ここにsqlを記述
 
 
     } catch (PDOException $e) {
