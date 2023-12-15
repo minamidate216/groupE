7dc59f42ae2611043d10be6b7f6308c8ff6fcc67
minamidate216 <2201412@s.asojuku.ac.jp>
2023-12-15 09:35:21 +0900

    Update ajax.js

--- script/ajax.js
+++ script/ajax.js
@@ -3,12 +3,8 @@ const app = new Vue({
     el:'#vueApp',
     //dataは　vueの中で使われる変数
     data:{
-        message:'',
         // 配列の変数を書いておく
         allData: productFromPHP,
-        subTotal : 0,
-        total: 0,
-        cartCount: 0
     }
     ,created() {
         // idごとに商品を格納するため　後ほどidを使用した処理が出てくるため
@@ -27,11 +23,9 @@ const app = new Vue({
         const index = this.getIndexBy(id);
         // 文字列のquantityを数値に変換する
         const quantity = parseInt(this.allData[index].quantity, 10); // 10は基数で、10進数を意味
-        if(this.allData[index].count < quantity){
+        if(this.allData[index].count < quantity && this.allData[index].count < 10){
                 this.allData[index].count++;
-            } else {
-        
-            }
+            } 
         },
         // 商品idが該当する商品の個数を減らす
         decrement(id) {
